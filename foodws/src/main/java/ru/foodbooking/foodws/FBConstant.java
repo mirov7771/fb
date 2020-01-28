5e7369c7711661f4049b50a8337efca968a14b57
mirov7771 <arturmirov777@gmail.com>
2020-01-28 15:18:32 +0300

    Constant class created2

--- foodws/src/main/java/ru/foodbooking/foodws/FBConstant.java
+++ foodws/src/main/java/ru/foodbooking/foodws/FBConstant.java
@@ -2,9 +2,9 @@ package ru.foodbooking.foodws;
 
 public class FBConstant {
 
-    public static Long TECHICAL_ERROR = 1001L;
-    public static Long BUSINESS_ERROR = 2001L;
-    public static String GET_TECHNICAL_ERROR = "Technical error";
-    public static String GET_BUSINESS_ERROR = "Business error";
+    public static Long CODE_TECHNICAL_ERROR = 1001L;
+    public static Long CODE_BUSINESS_ERROR = 2001L;
+    public static String MESSAGE_TECHNICAL_ERROR = "Technical error";
+    public static String MESSAGE_BUSINESS_ERROR = "Business error";
 
 }
