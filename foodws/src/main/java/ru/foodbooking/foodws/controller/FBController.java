ed94a5d98539c5270546373d9b93db83428b384e
mirov7771 <arturmirov777@gmail.com>
2020-01-28 17:07:51 +0300

    Exception1 is created

--- foodws/src/main/java/ru/foodbooking/foodws/controller/FBController.java
+++ foodws/src/main/java/ru/foodbooking/foodws/controller/FBController.java
@@ -2,6 +2,7 @@ package ru.foodbooking.foodws.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.foodbooking.foodws.FBConstant;
 import ru.foodbooking.foodws.FBException;
 import ru.foodbooking.foodws.services.FBServices;
 import ru.foodbooking.foodws.support.request.FBServicesReq;
@@ -18,8 +19,11 @@ public class FBController {
     @RequestMapping(method = RequestMethod.GET, value = "/fb")
     @ResponseBody
     public FBServicesRes handleRequest(@RequestParam("method") String method,
-                                       @RequestBody FBServicesReq req) throws FBException {
+                                       @RequestBody FBServicesReq req)
+            throws FBException {
         FBServices service = fbServices.get(method);
+        if (service == null)
+            throw new FBException(FBConstant.CODE_TECHNICAL_ERROR, FBConstant.MESSAGE_TECHNICAL_ERROR);
         service.execute(null);
         return null;
     }
