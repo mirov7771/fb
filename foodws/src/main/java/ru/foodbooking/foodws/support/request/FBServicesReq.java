189a58766af6d0e37998b84327664b5ab7ad4fa7
mirov7771 <arturmirov777@gmail.com>
2020-01-29 11:11:52 +0300

    setters & getters into FBServicesReq

--- foodws/src/main/java/ru/foodbooking/foodws/support/request/FBServicesReq.java
+++ foodws/src/main/java/ru/foodbooking/foodws/support/request/FBServicesReq.java
@@ -1,4 +1,42 @@
 package ru.foodbooking.foodws.support.request;
 
 public class FBServicesReq {
+
+    private Long pointid;
+    private String address;
+    private Long ctgrid;
+    private Long productid;
+
+    public void setPointid(Long pointid) {
+        this.pointid = pointid;
+    }
+
+    public Long getPointid() {
+        return pointid;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setCtgrid(Long ctgrid) {
+        this.ctgrid = ctgrid;
+    }
+
+    public Long getCtgrid() {
+        return ctgrid;
+    }
+
+    public void setProductid(Long productid) {
+        this.productid = productid;
+    }
+
+    public Long getProductid() {
+        return productid;
+    }
 }
+
