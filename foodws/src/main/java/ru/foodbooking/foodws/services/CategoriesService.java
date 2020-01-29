9a5eb8c542506ba7134add8a8d8b7148616ba6d8
mirov7771 <arturmirov777@gmail.com>
2020-01-29 11:38:35 +0300

    setters & getters into FBServicesReq1

--- foodws/src/main/java/ru/foodbooking/foodws/services/CategoriesService.java
+++ foodws/src/main/java/ru/foodbooking/foodws/services/CategoriesService.java
@@ -2,12 +2,14 @@ package ru.foodbooking.foodws.services;
 
 import org.springframework.stereotype.Component;
 import ru.foodbooking.foodws.FBException;
+import ru.foodbooking.foodws.support.request.FBServicesReq;
+import ru.foodbooking.foodws.support.response.FBServicesRes;
 
 @Component("categories")
 public class CategoriesService implements FBServices{
 
     @Override
-    public String execute(String request) throws FBException {
+    public FBServicesRes execute(FBServicesReq request) throws FBException {
         return null;
     }
 }
